0aa78251938a20a82f4c7ff591c0360e76896747
Velarken <181585418+Velarken@users.noreply.github.com>
2025-09-14 14:17:11 -0400

    completed tests for all class methods

--- src/__tests__/ships.test.js
+++ src/__tests__/ships.test.js
@@ -8,3 +8,16 @@ test('Does the Ship class return the correct object', () => {
         sunken:false
     })
 })
+test('hit method should increment by 1', () => {
+    let sub = new Ship('submarine',3)
+    sub.hit()
+    sub.hit()
+    expect(sub.hitsTaken).toEqual(2)
+})
+test('isSunk method should alert when reaching max hits', () => {
+    let sub = new Ship('submarine',3)
+    sub.hit()
+    sub.hit()
+    sub.hit()
+    expect(sub.sunken).toEqual(false)
+})
